f1125ff3d366cff2af41dc6630b8211bf0fb43c1
PrathamJoshi <129665552+Prathamjoshi355@users.noreply.github.com>
2023-10-12 23:49:35 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,11 +1,1 @@
-from googletrans import Translator
 
-def translate_english_to_hindi(text):
-    translator = Translator()
-    translation = translator.translate(text, src='en', dest='hi')
-    return translation.text
-
-if __name__ == "__main__":
-    input_text = input("Enter text in English: ")
-    translated_text = translate_english_to_hindi(input_text)
-    print(f"Translation in Hindi: {translated_text}")
